a796daf28062885c079160fe194f500d7c804eee
Wasuwat Limsuparhat <wasuwat.limsuparhat@allianz.com>
2019-03-06 10:31:16 +0700

    add mock content

--- index.tsx
+++ index.tsx
@@ -10,15 +10,22 @@ export default class App extends Component<{}, {}> {
             <Fragment>
                 <MetaData></MetaData>
                 <Header></Header>
-                <main>
-                    <h4>Hello React + TypeScript</h4>
-                    <section>
-                        <p>
-                            Zero abstracts the usual project configuration for routing, bundling, and transpiling to make it easier to get started.
-                        </p>
-                    </section>
-                </main>
+                <Content></Content>
             </Fragment>
         );
     }
 }
+
+const Content = () => (
+    <main>
+        <h4>Hello React + TypeScript</h4>
+        <section>
+            <p>
+                Zero abstracts the usual project configuration for routing, bundling, and transpiling to make it easier to get started.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam enim quam unde optio cupiditate nisi? Aliquid excepturi cupiditate aspernatur, blanditiis nisi corrupti sunt consequuntur cum saepe aut impedit fuga dicta vitae quas odit? Alias, ipsam ex fuga, incidunt hic officiis architecto ipsum, sed sequi quod eos possimus totam non neque laboriosam tempora quasi reprehenderit unde dolorem qui eum sapiente quaerat voluptatibus? Vel, dolorum? Laudantium id labore exercitationem odit vitae quia, doloribus quas voluptatum magni facilis quasi cumque tenetur fugiat minima. Nisi a delectus sed rem doloremque ex itaque illum deleniti nemo, ratione ut asperiores dicta, quidem optio labore! Eligendi, sit.
+            </p>
+        </section>
+    </main>
+)
